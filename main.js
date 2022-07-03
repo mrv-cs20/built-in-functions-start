2bc8bc9b7bfeb1d2ebfaf16ab6ed28e328853561
Colin <37160046+mrveldkamp@users.noreply.github.com>
2022-07-02 22:31:02 -0600

    prepare start code

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("hi");
+// LESSONS FROM BUILT-IN FUNCTIONS
+
+// For each built-in function, consider:
+// - the purpose of the function
+// - what data is passed into the function
+// - what data is returned from the function
+
+// ALERT()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// PROMPT()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// CONFIRM()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// CONSOLE.LOG()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// MATH.RANDOM()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// MATH.SQRT()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
+
+// DOCUMENT.GETELEMENTBYID()
+// Purpose: 
+// Data Passed In:
+// Data Returned:
