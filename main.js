0550bdd2c9e3e6b8bc604f518e2f308224cfd1cf
mrveldkamp <colin.veldkamp@epsb.ca>
2022-07-04 07:25:28 -0600

    update start code

--- main.js
+++ main.js
@@ -5,37 +5,68 @@
 // - what data is passed into the function
 // - what data is returned from the function
 
-// ALERT()
-// Purpose: 
-// Data Passed In:
-// Data Returned:
-
-// PROMPT()
-// Purpose: 
-// Data Passed In:
-// Data Returned:
-
-// CONFIRM()
-// Purpose: 
-// Data Passed In:
-// Data Returned:
-
+// **************************
 // CONSOLE.LOG()
-// Purpose: 
+// Purpose:
 // Data Passed In:
 // Data Returned:
+// **************************
+// console.log("Hello Console");
 
+// **************************
+// ALERT()
+// Purpose:
+// Data Passed In:
+// Data Returned:
+// **************************
+// alert("Hello World");
+
+// **************************
+// PROMPT()
+// Purpose:
+// Data Passed In:
+// Data Returned:
+// **************************
+// let response = prompt("This is a question?");
+// console.log(`This is your reponse: ${response}.`);
+
+// **************************
+// CONFIRM()
+// Purpose:
+// Data Passed In:
+// Data Returned:
+// **************************
+// let confirmed = confirm("Are you sure you want to do this?");
+// console.log(confirmed);
+// if (confirmed) {
+//   console.log("Okay, here we go!");
+// } else {
+//   console.log("Alright, maybe later...");
+// }
+
+// **************************
 // MATH.RANDOM()
-// Purpose: 
+// Purpose:
 // Data Passed In:
 // Data Returned:
+// **************************
+// let randNum = Math.random();
+// console.log(randNum);
 
+// **************************
 // MATH.SQRT()
-// Purpose: 
+// Purpose:
 // Data Passed In:
 // Data Returned:
+// **************************
+// let answer = Math.sqrt(25);
+// console.log(answer);
 
+// **************************
 // DOCUMENT.GETELEMENTBYID()
-// Purpose: 
+// Purpose:
 // Data Passed In:
 // Data Returned:
+// **************************
+// let mainH1 = document.getElementById("heading");
+// console.log(mainH1);
